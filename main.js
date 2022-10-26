99af16438620f811bf23fddd5c00d46a8a8d486e
lebron.xia <1031505541@qq.com>
2022-10-26 14:23:17 +0800

    根据electron官网进行学习

--- main.js
+++ main.js
@@ -1,26 +1,13 @@
-let electron = require('electron')
-let app = electron.app  //引用app
-let BrowserWindow = electron.BrowserWindow   //窗口
-let mainWindow = null  //打开的主窗口
-let BrowserView = electron.BrowserView
-app.on('ready', () => {
-  mainWindow = new BrowserWindow({
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
     width: 800,
-    height: 800,
-    webPreferences: {nodeIntegration:true,contextIsolation: false,enableRemoteModule:true }
-  })
-  require('./main/main')
-  require("@electron/remote/main").initialize();
-  require("@electron/remote/main").enable(mainWindow.webContents);
-  let view = new BrowserView()
-  mainWindow.setBrowserView(view)
-  view.setBounds({ x: 0, y: 120, width: 1000, height: 680 })
-  view.webContents.loadURL('https://electronjs.org')
-  mainWindow.loadFile('index.html')
-  mainWindow.openDevTools()
-  mainWindow.on('close', () => {
-    mainWindow = null
+    height: 600,
+  });
+  win.loadFile("index.html");
+};
 
-  })
-
-})
+app.whenReady().then(() => {
+  createWindow();
+});
